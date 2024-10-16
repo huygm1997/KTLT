09efad70a173fe65893f6915b77cec3936e4b3a7
huygm1997 <95552014+huygm1997@users.noreply.github.com>
2024-10-16 13:23:11 +0700

    Add files via upload

---
+++ Text1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Text1 {
+    public static String gioiTinh(boolean gt) {
+        return null;
+    }
+    public static void nhapThongtinsinhvien() {
+        Scanner sc = new Scanner(System.in);
+        String maSinhvien, hoTen, ngaySinh, lop, khoa, diaChi;
+        int tuoi;
+        boolean gt;
+        System.out.println("Nhập mã số sinh viên: ");
+        maSinhvien = sc.nextLine();
+        System.out.println("Nhập họ và tên sinh viên: ");
+        hoTen = sc.nextLine();
+        System.out.println("Nhập ngày sinh: ");
+        ngaySinh = sc.nextLine();
+        System.out.println("Nhập tên lớp: ");
+        lop = sc.nextLine();
+        System.out.println("Nhập tên khoa: ");
+        khoa = sc.nextLine();
+        System.out.println("Nhập địa chỉ: ");
+        diaChi = sc.nextLine();
+        System.out.println("Nhập tuổi: ");
+        tuoi = sc.nextInt();
+        System.out.println("Nhập giới tính ");
+        gt = sc.nextBoolean();
+    
+        
+    }
+        
+    }
+   
+
+
