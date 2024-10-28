066c02319cf2820cecc058302801bb185f55591f
huygm1997 <95552014+huygm1997@users.noreply.github.com>
2024-10-28 17:56:43 +0700

    Update buoi4.java

--- buoi4.java
+++ buoi4.java
@@ -1,1 +1,21 @@
-ãi c"ỉa··
+public static void demo3(int n) {
+        System.out.printf("\nDanh sách các phần tử n = %d là:\n", n); 
+        for (int i = 1; i <= n; i++) { 
+            if (i % 2 == 0) {
+                System.out.println(i + " là số chẵn!");
+            } else {
+                System.out.println(i + " là số lẻ");
+            }
+            System.out.println("\t" + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num3;
+        System.out.println("\nNhập giá trị : ");
+        num3 = sc.nextInt();
+        demo3(num3);
+        sc.close();
+    }
+}
