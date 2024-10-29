0c7ef08b5b49488078748c954d95c45aeb4992de
huygm1997 <95552014+huygm1997@users.noreply.github.com>
2024-10-29 23:02:59 +0700

    Update buoi4.java

--- buoi4.java
+++ buoi4.java
@@ -1,27 +1,65 @@
-package demo1;
+package baitapbuoi4;
 
 import java.util.Scanner;
 
-public class Demo {
-    public static void demo3(int n) {
-        System.out.printf("\nDanh sách các phần tử n = %d là:\n", n); 
-        for (int i = 1; i <= n; i++) { 
-            if (i % 2 == 0) {
-                System.out.println(i + " là số chẵn!");
-            } else {
-                System.out.println(i + " là số lẻ");
-            }
-            System.out.println("\t" + i);
+public class Baitapbuoi4 {
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nNhập giá trị : ");
+        int n = sc.nextInt();
+
+        System.out.println("bài2======");
+        bai2(n);
+        System.out.println("bài3======");
+        bai3(n);
+        System.out.println("bài4======");
+        bai4(n);
+        System.out.println("bài5======");
+        bai5(n);
+        System.out.println("bài6======");
+        bai6(n);
+        
+
+        sc.close();
+    }
+
+    public static void bai2(int n) {
+        System.out.println(n);
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
         }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num3;
-        System.out.println("\nNhập giá trị : ");
-        num3 = sc.nextInt();
-        demo3(num3);
-        sc.close();
+    public static void bai3(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) { 
+                System.out.println(i);
+            }
+        }
     }
-}
 
+    public static void bai4(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) { 
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void bai5(int n) {
+        for (int i = 1; i < n; i++) {
+            if (i % 2 == 1) { 
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void bai6(int n) {
+        for (int i = 1; i < n; i++) {
+            if (i % 2 == 0) { 
+                System.out.println(i);
+            }
+        }
+    }
+
+}
